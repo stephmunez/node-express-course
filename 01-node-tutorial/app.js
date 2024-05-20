4906c4857815abd510fe03fa893d481f0dc5d876
Stephen Muñez <sjtmunez@gmail.com>
2024-05-20 17:25:42 +0800

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,36 +1,21 @@
-const { readFile, writeFile } = require('fs').promises;
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+// get back the class
+// if want custom extend from class
+// otherwise just for emitting and handling events create instance
+const EventEmitter = require('events');
 
-const start = async () => {
-  try {
-    const first = await readFile('./content/first.txt', 'utf8');
-    const second = await readFile('./content/second.txt', 'utf8');
-    await writeFile(
-      './content/result-mind-grenade.txt',
-      `THIS IS AWESOME : ${first} ${second}`,
-      { flag: 'a' }
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
+const customEmitter = new EventEmitter();
 
-start();
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, 'utf8', (err, data) => {
-//       if (err) {
-//         reject(err)
-//       } else {
-//         resolve(data)
-//       }
-//     })
-//   })
-// }
-// getText('./content/first.txt')
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err))
+customEmitter.on('response', (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`);
+});
+
+customEmitter.on('response', () => {
+  console.log('some other logic here');
+});
+
+customEmitter.emit('response', 'john', 34);
