bfb1beb107bb2ca2654c7bd6121da81149dbf7f9
Stephen Muñez <sjtmunez@gmail.com>
2024-05-23 13:26:48 +0800

    Update app.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,13 +1,16 @@
-const { createReadStream } = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-// default 64kb
-// last buffer - remainder
-// highWaterMark - control size
-// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
-// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('./content/big.txt');
-
-stream.on('data', (result) => {
-  console.log(result);
-});
-stream.on('error', (err) => console.log(err));
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+    fileStream.on('open', () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on('error', (err) => {
+      res.end(err);
+    });
+  })
+  .listen(5001);
