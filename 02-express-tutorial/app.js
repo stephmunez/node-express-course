cd162a2cbe0815fc5c7ebb532a7471e5056c9bd4
Stephen Muñez <sjtmunez@gmail.com>
2024-06-03 13:04:07 +0800

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('user hit the resource');
-  res.status(200).send('Home Page');
-});
+// setup static and middleware
+app.use(express.static('./public'));
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page');
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>');
+  res.status(404).send('resource not found');
 });
 
-app.listen(5001, () => {
-  console.log('server is listening on port 5001...');
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....');
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
