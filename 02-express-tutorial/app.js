c9de97d84fa4d9d72fc8d8997af26c2489e6fa2c
Stephen Muñez <sjtmunez@gmail.com>
2024-06-06 14:25:41 +0800

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// setup static and middleware
-app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-//   adding to static assets
-//   SSR
-// })
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found');
+const { products } = require('./data');
+app.get('/', (req, res) => {
+  res.json(products);
 });
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....');
+app.listen(5001, () => {
+  console.log('Server is listening on port 5001....');
 });
